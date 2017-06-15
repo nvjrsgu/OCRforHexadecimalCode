e0004331939f397f42cb3f54ad485e119d142ea4
nvjrsgu <nvjrsgu@gmail.com>
2017-06-15 21:02:34 +0300

    Added methods for more comfortable tests

--- src/com/nvjrsgu/Main.java
+++ src/com/nvjrsgu/Main.java
@@ -4,38 +4,32 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.LinkedHashSet;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedImage code1 = ImageIO.read(new FileInputStream("./data/codeExamples/1.jpg"));
-        BufferedImage code2 = ImageIO.read(new FileInputStream("./data/codeExamples/2.jpg"));
-        //ImageIO.write(code1,"png", new File("./data/codeExamples/1_1.jpg"));
-        CutLines cutLines = new CutLines(code1);
-        CutLines cutLines2 = new CutLines(code2);
 
-        code1 = cutLines.cropLines();
-        code2 = cutLines2.cropLines();
-        ImageIO.write(code1,"png", new File("./data/codeExamples/1_2.jpg"));
-        ImageIO.write(code2,"png", new File("./data/codeExamples/2_2.jpg"));
+        cutLinesTest("./data/codeExamples/1", "jpg");
+        cutLinesTest("./data/codeExamples/2", "jpg");
+        cutLinesTest("./data/codeExamples/3", "jpg");
 
-        LinkedHashSet<BufferedImage> images = cutLines.getLines();
-        int counter = 1;
-        for(BufferedImage b: images){
-            ImageIO.write(b,"png", new File("./data/codeExamples/1_3_"+counter+".jpg"));
-            counter++;
+    }
+
+    public static void cutLinesTest(String pathImage, String format){
+        try {
+            BufferedImage image = ImageIO.read(new FileInputStream(pathImage+"."+format));
+            CutLines cutLines = new CutLines(image);
+            LinkedHashSet<BufferedImage> images = cutLines.cropLines();
+            int counter = 1;
+            for (BufferedImage b : images) {
+                ImageIO.write(b, "png", new File(pathImage + "_line_" + counter + ".png"));
+                counter++;
+            }
+        }catch (IOException e){
+            System.out.println(e);
         }
 
-         images = cutLines2.getLines();
-         counter = 1;
-        for(BufferedImage b: images){
-            ImageIO.write(b,"png", new File("./data/codeExamples/2_3_"+counter+".jpg"));
-            counter++;
-        }
     }
 }
