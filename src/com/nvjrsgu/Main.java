25900b8f7006e7f3f6ad1903a3ea11fa860b1fca
nvjrsgu <nvjrsgu@gmail.com>
2017-06-19 13:48:45 +0300

    Added method for testing DrawLines class

--- src/com/nvjrsgu/Main.java
+++ src/com/nvjrsgu/Main.java
@@ -11,11 +11,41 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        cutLinesTest("./data/codeExamples/1", "jpg");
+      /*  cutLinesTest("./data/codeExamples/1", "jpg");
         cutLinesTest("./data/codeExamples/2", "jpg");
         cutLinesTest("./data/codeExamples/3", "jpg");
+*/
+       /* cutSymbolsTest("./data/codeExamples/3_line_1", "png");
+        cutSymbolsTest("./data/codeExamples/3_line_2", "png");
+        cutSymbolsTest("./data/codeExamples/3_line_3", "png");
+        cutSymbolsTest("./data/codeExamples/1_line_1", "png");
+        cutSymbolsTest("./data/codeExamples/1_line_2", "png");
+        cutSymbolsTest("./data/codeExamples/1_line_3", "png");
+        cutSymbolsTest("./data/codeExamples/2_line_1", "png");
+        cutSymbolsTest("./data/codeExamples/2_line_2", "png");
+        cutSymbolsTest("./data/codeExamples/2_line_3", "png");
+*/
+
+       drawLineTest("./data/codeExamples/3", "jpg");
 
     }
+
+    public static void drawLineTest(String pathImage, String format){
+        DrawLines drawLines = new DrawLines();
+        LinkedHashSet<Integer> link = new LinkedHashSet<>();
+        try {
+            BufferedImage image = ImageIO.read(new FileInputStream(pathImage + "." + format));
+            link.add(image.getHeight() / 2);
+            drawLines.drawLines(link, image, 0x4AA449, false);
+            link.clear();
+            link.add(image.getWidth()/2);
+            drawLines.drawLines(link, image, 0x4AA449, true);
+            ImageIO.write(image, "png", new File(pathImage + "_drewLines" + ".png"));
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+
 
     public static void cutLinesTest(String pathImage, String format){
         try {
@@ -31,5 +61,24 @@ public class Main {
             System.out.println(e);
         }
 
+    }
+
+    public static void cutSymbolsTest(String pathImage, String format){
+        try {
+            BufferedImage image = ImageIO.read(new FileInputStream(pathImage + "." + format));
+            CutSymbols cutSymbols = new CutSymbols(image);
+            LinkedHashSet<Integer> whereCut = cutSymbols.getMinimums();
+            DrawLines drawLines = new DrawLines();
+            //drawLines.drawVerticalLines(whereCut, image, 0x34C6C2);
+            //ImageIO.write(image, "png", new File(pathImage + "_draw2_" + ".png"));
+
+            whereCut = cutSymbols.cleanMinimums(whereCut);
+            drawLines.drawLines(whereCut, image, 0x4AA449, true);
+            ImageIO.write(image, "png", new File(pathImage + "_draw2" + ".png"));
+
+
+        }catch (IOException e){
+            System.out.println(e);
+        }
     }
 }
