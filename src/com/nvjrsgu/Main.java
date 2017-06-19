0eb0a779d1df0180f3c6ea6af052e2bf43cb9273
nvjrsgu <nvjrsgu@gmail.com>
2017-06-19 20:12:16 +0300

    Testing seam carving methods

--- src/com/nvjrsgu/Main.java
+++ src/com/nvjrsgu/Main.java
@@ -4,13 +4,124 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.LinkedHashSet;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static int[] massSum() throws Exception {
+        TextImage image = new TextImage(ImageIO.read(new FileInputStream("./data/codeExamples/2x4_energ.png")));
+        int mass[][] = image.getImageColorArray();
+        /*int mass[][] = {
+                {1, 2, 3, 4, 5, 6},
+                {3, 4, 3, 4, 3, 4},
+                {5, 6, 7, 8, 7, 6},
+                {1, 2, 3, 4, 5, 6},
+                {3, 4, 3, 4, 3, 4},
+                {5, 6, 7, 8, 7, 6}
+        };*/
 
+        int height = mass.length;
+        int width = mass[0].length;
+
+        for(int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                mass[y][x] &= 0x00FFFFFF;
+            }
+        }
+        /*for(int[] i: mass){
+            for(int j: i){
+                System.out.print(j+", ");
+            }
+            System.out.println();
+        }*/
+
+        int mass2[][] = new int[height][width];
+        // System.out.println(mass2.length+" "+mass.length);
+        for(int x = 0; x < width; x++){
+            mass2[0][x] = mass[0][x];
+        }
+        for(int x = 0; x < height; x++){
+            mass2[x][0] = Integer.MAX_VALUE;
+            mass2[x][width-1] = Integer.MAX_VALUE;
+        }
+        for(int y = 1; y < height; y++){
+            for(int x = 1; x < width-1; x++){
+                int x1 = mass2[y-1][x-1];
+                int x2 = mass2[y-1][x];
+                int x3 = mass2[y-1][x+1];
+
+                if(x1 < x2){
+                    if(x1 < x3){
+                        mass2[y][x] = mass[y][x]+x1;
+                    }else{
+                        mass2[y][x] = mass[y][x]+x3;
+                    }
+                }else{
+                    if(x2 < x3){
+                        mass2[y][x] = mass[y][x]+x2;
+                    }else{
+                        mass2[y][x] = mass[y][x]+x3;
+                    }
+                }
+            }
+        }
+        int t[] = new int[height];
+        int min = Integer.MAX_VALUE;
+
+            for(int f = 0; f < width; f++){
+                if(mass2[height-1][f] < min){
+                    t[height-1] = f;
+                    min = mass2[height-1][f];
+                }
+            }
+            min = Integer.MAX_VALUE;
+
+
+        for(int i = height-2; i > 0; i--){
+            for(int f = t[i+1]-1; f <= t[i+1]+1; f++){
+                if(mass2[i][f] < min){
+                    t[i] = f;
+                    min = mass2[i][f];
+                }
+            }
+            min = Integer.MAX_VALUE;
+        }
+
+
+        return t;
+      /*  for(int x = 0; x < height; x++){
+            mass2[x][0] = 0;
+            mass2[x][width-1] = 0;
+        }*/
+        // System.out.println("________________________________________");
+      /*  for(int[] i: mass2){
+            for(int j: i){
+                System.out.print(j+", ");
+            }
+            System.out.println();
+        }*/
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        int[] k = massSum();
+
+        BufferedImage image = ImageIO.read(new FileInputStream("./data/codeExamples/2x4.png"));
+        int height = image.getHeight();
+        int width = image.getWidth();
+
+        for(int y = 0; y < height; y++){
+            for(int x = 0; x < width; x++){
+                image.setRGB(x,y,image.getRGB(x,y));
+                if(x == k[y]){
+                    image.setRGB(x,y,0xFF0000);
+                }
+            }
+        }
+
+        ImageIO.write(image, "png", new File("./data/codeExamples/2x4_.png"));
       /*  cutLinesTest("./data/codeExamples/1", "jpg");
         cutLinesTest("./data/codeExamples/2", "jpg");
         cutLinesTest("./data/codeExamples/3", "jpg");
@@ -26,7 +137,15 @@ public class Main {
         cutSymbolsTest("./data/codeExamples/2_line_3", "png");
 */
 
-       drawLineTest("./data/codeExamples/3", "jpg");
+/*
+      // drawLineTest("./data/codeExamples/3", "jpg");
+        ImageEnergy energy = new ImageEnergy();
+        ImageScaling scaling = new ImageScaling();
+        BufferedImage image = ImageIO.read(new FileInputStream("./data/codeExamples/2x4.png"));
+       image = energy.getImageEnergy(image);
+        //image = scaling.imageX2(image, 4);
+        ImageIO.write(image, "png", new File("./data/codeExamples/2x4_energ.png"));
+*/
 
     }
 
